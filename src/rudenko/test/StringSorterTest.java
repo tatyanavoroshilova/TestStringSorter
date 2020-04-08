3b37a134cf36c0bb4affa67c30a61a45bc1d24d6
Tatiana Rudenko <voroshilova.t.f@gmail.com>
2020-04-08 18:04:16 +0300

    added StringSorter

---
+++ src/rudenko/test/StringSorterTest.java
@@ -0,0 +1,13 @@
+package rudenko.test;
+
+import org.junit.Test;
+import rudenko.testtask.StringSorter;
+
+public class StringSorterTest {
+    @Test
+    public void test1() {
+        StringSorter stringSorter = new StringSorter();
+
+
+    }
+}
